8e53cc541c12ff6eb1c33715a1b534fedafe0752
swwinslow <swwinslow@gmail.com>
2016-10-31 01:00:08 -0400

    Set Up

---
+++ src/main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int quantum = 0;
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Round Robin (1) | First Come, First Serve (2) | Shortest Job First (3)");
+		
+		System.out.print("Please enter the CPU Scheduling: ");
+		String scheduler = in.nextLine();
+		
+		System.out.println(scheduler);
+		if(scheduler.equals("1")){
+			System.out.println("we are going to round robin");
+			System.out.println("Please enter the Quantum Number: ");
+			quantum = Integer.parseInt(in.nextLine());
+
+		} else if(scheduler.equals("2")){
+			System.out.println("we are going to FCFS");
+		} else if (scheduler.equals("3")){
+			System.out.println("we are going to SJF");
+		}
+		
+		System.out.println("Job information");
+		
+		int[] ID = new int[10];
+		int[] bursts = new int[10];
+		
+		String exitCode = "Y";
+		int count = -1;
+		
+		do{
+			count++;
+			System.out.print("Please enter the job ID: " );
+			int jobID = Integer.parseInt(in.nextLine());
+			ID[count] = jobID;
+			
+			System.out.print("Please enter the burst time for Job #" + jobID);
+			int jobBurst = Integer.parseInt(in.nextLine());
+			bursts[count] = jobBurst;
+			
+			System.out.print("Contine: Y | Exit: X");
+			exitCode = in.nextLine();
+			
+		}while(exitCode.equals("Y"));
+		
+		for (int i = 0; i < ID.length; i++){
+			System.out.println(ID[i]);
+			System.out.println(bursts[i]);
+
+		}
+		
+		
+	
+		
+		
+	
+		
+
+
+	}
+
+}
